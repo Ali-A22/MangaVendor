b67e3514243f2541a777b87f4eeb7a48e21389a2
Ali Askari <aliaskari31@gmail.com>
2023-11-26 23:50:25 +0000

    fixing bugs

--- server/index.js
+++ server/index.js
@@ -3,33 +3,27 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import path from 'path';
 
 import postRoutes from './routes/posts.js';
 
 const app = express();
 dotenv.config();
 
-app.use(bodyParser.json({ limit: '30mb', extended: true }));
-app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
+const corsOptions = {
+  origin: "https://mangavendor.onrender.com",
+}
+app.use(cors(corsOptions));
 
 
-app.use(cors({
-  origin: 'https://mangavendor.onrender.com',
-  credentials: true,
-}));
+app.use(bodyParser.json({ limit: '30mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+
 
 app.use('/posts', postRoutes);
 
-app.use(express.static('build'));
+const PORT = process.env.PORT|| 5000;
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
-
-const PORT = process.env.PORT || 5000;
-
-mongoose
-  .connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: ${PORT}`)))
   .catch((error) => console.log(`${error} did not connect`));
+
