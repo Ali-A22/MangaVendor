d3ca3ef09972c00317de0f20a3db7f44677d9426
Ali Askari <aliaskari31@gmail.com>
2023-11-27 00:23:49 +0000

    bug fix

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ const corsOptions = {
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
-app.use(cors());
+app.use(cors(corsOptions));
 
 app.use('/posts', postRoutes);
 
@@ -25,3 +25,7 @@ const PORT = process.env.PORT|| 5000;
 mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: ${PORT}`)))
   .catch((error) => console.log(`${error} did not connect`));
+
+  app.get("/", (req, res) => {
+    res.status(201).json({message: "Connected to Backend!"});
+});
